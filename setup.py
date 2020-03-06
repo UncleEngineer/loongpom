9f9fa41fb446a9db9d860ae175f9782500d88c06
Uncle Engineer <eng.imron@gmail.com>
2020-03-06 21:13:44 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from distutils.core import setup
+import os.path
+
+def get_file(*paths):
+    path = os.path.join(*paths)
+    try:
+        with open(path, 'rb') as f:
+            return f.read().decode('utf8')
+    except IOError:
+        pass
+
+def get_readme():
+    return get_file(os.path.dirname(__file__), 'README.rst')
+
+
+
 setup(
   name = 'loongpom',         # How you named your package folder (MyLib)
   packages = ['loongpom'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Show Loong Pom Name Jaaa',   # Give a short description about your library
+  long_description='plese read in: https://github.com/UncleEngineer/loongpom',
+  long_description_content_type='text/x-rst',
   author = 'Loong Wissawakorn',                   # Type in your name
   author_email = 'loong.wissawakorn2@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/UncleEngineer/loongpom',   # Provide either the link to your github or to your website
